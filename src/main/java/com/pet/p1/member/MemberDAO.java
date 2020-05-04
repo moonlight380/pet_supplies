78e47e7c5510007b07a180552bbbb9a9223184c4
SOOKYOUNG-KIM <63095409+SOOKYOUNG-KIM@users.noreply.github.com>
2020-05-05 01:57:17 +0900

    commit

--- src/main/java/com/pet/p1/member/MemberDAO.java
+++ src/main/java/com/pet/p1/member/MemberDAO.java
@@ -1,5 +1,51 @@
 package com.pet.p1.member;
 
+
+import java.util.List;
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.pet.p1.util.Pager;
+
+@Repository
 public class MemberDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.hani.s5.member.MemberDAO.";
+	
+	public Long memberCount(Pager memberPager) throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"memberCount", memberPager);
+	}
+	
+	public List<MemberVO> memberList(Pager memberPager)throws Exception{
+		return sqlSession.selectList(NAMESPACE+"memberList", memberPager);
+	}
+	
+	public MemberVO memberLogin(MemberVO memberVO)throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"memberLogin", memberVO);
+	}
+	
+	public int memberJoin(MemberVO memberVO)throws Exception{
+		return sqlSession.insert(NAMESPACE+"memberJoin", memberVO);
+	}
+	
+	public int memberUpdate(MemberVO memberVO)throws Exception{
+		return sqlSession.update(NAMESPACE+"memberUpdate", memberVO);
+	}
+	
+	public int memberDelete(MemberVO memberVO)throws Exception{
+		return sqlSession.delete(NAMESPACE+"memberDelete", memberVO);
+	}
+	
+	public MemberVO memberIdCheck(MemberVO memberVO)throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"memberIdCheck",memberVO);
+	}
+	
+	public int memberDeletes(List<String> list)throws Exception{
+		return sqlSession.delete(NAMESPACE+"memberDeletes", list);
+	}
 
 }
---
+++ src/main/java/com/pet/p1/member/MemberService.java
@@ -0,0 +1,55 @@
+package com.pet.p1.member;
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import com.pet.p1.util.Pager;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	public List<MemberVO> memberList(Pager memberPager)throws Exception{
+		memberPager.makeRow();
+		long totalCount = memberDAO.memberCount(memberPager);
+		memberPager.makePage(totalCount);
+		return memberDAO.memberList(memberPager);
+	}
+
+	public int memberUpdate(MemberVO memberVO)throws Exception{
+		return memberDAO.memberUpdate(memberVO);
+	}
+	
+	public int memberDelete(MemberVO memberVO) throws Exception{
+		return memberDAO.memberDelete(memberVO);
+	}
+	
+	public MemberVO memberLogin(MemberVO memberDTO)throws Exception{
+		return memberDAO.memberLogin(memberDTO);
+	}
+	
+	public int memberJoin(MemberVO memberVO, MultipartFile avatar, HttpSession session)throws Exception{
+		return memberDAO.memberJoin(memberVO);
+	}
+
+	
+	public MemberVO memberIdCheck(MemberVO memberVO)throws Exception{
+		return memberDAO.memberIdCheck(memberVO);
+	}
+	
+	public int memberDeletes(List<String> list)throws Exception{
+		return memberDAO.memberDeletes(list);
+	}
+	
+
+}
